bd85fe4d2ba4a31471f26aba84db38a62f14161d
jawad Bin Azam <abidibnazam@gmail.com>
2023-06-27 23:10:49 +0600

    about me 50% done

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsArrowRightShort } from "react-icons/bs";
 
 const AboutMe = () => {
   return (
@@ -14,6 +15,40 @@ const AboutMe = () => {
             decided to create a custom blogging site where everyone can post
             there writings and share those things on different platfroms.
           </p>
+          <p className="my-5">
+            Here are few technologies that I've been working with<br></br>{" "}
+            reccently:
+          </p>
+          <div className="flex gap-10">
+            <ul className="text-sm">
+              <li className="my-2">
+                <BsArrowRightShort className="inline-block text-red-400" />{" "}
+                JavaScript (ES6+)
+              </li>
+              <li className="my-2">
+                <BsArrowRightShort className="inline-block text-red-400" />{" "}
+                React.js
+              </li>
+              <li className="my-2">
+                <BsArrowRightShort className="inline-block text-red-400" />{" "}
+                Node.js
+              </li>
+            </ul>
+            <ul className="text-sm">
+              <li className="my-2">
+                <BsArrowRightShort className="inline-block text-red-400" />{" "}
+                MongoDB
+              </li>
+              <li className="my-2">
+                <BsArrowRightShort className="inline-block text-red-400" />{" "}
+                Framer Motion
+              </li>
+              <li className="my-2">
+                <BsArrowRightShort className="inline-block text-red-400" />{" "}
+                Firebase
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
