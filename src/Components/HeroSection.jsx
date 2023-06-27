51891717ceb30aac863a78cc723ecaf12e912848
jawad Bin Azam <abidibnazam@gmail.com>
2023-06-27 20:16:12 +0600

    hero section

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -3,11 +3,16 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="lg:mt-14 mt-10">
-      <h1 className="">Hi, my name is</h1>
+      <h1 className="text-slate-500">Hi, my name is</h1>
       <h1 className="tracking-tight text-7xl font-bold mt-8">Jawad Bin Azam</h1>
       <h1 className="tracking-tighter text-7xl font-extrabold text-slate-800">
         I create wonders for the<br></br>web.
       </h1>
+      <p className="w-1/2">
+        I'm a webdeveloper specializing in building exceptional digital
+        experience. As a mern stack developer I'm focused to build sleek and
+        optimized web applications.
+      </p>
     </div>
   );
 };
