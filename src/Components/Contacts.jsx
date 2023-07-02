0cebdd8ba0e69c394241231320036a01cbab3ff2
jawad Bin Azam <abidibnazam@gmail.com>
2023-07-02 14:13:31 +0600

    email js set up

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -5,6 +5,7 @@ import linkedin from "../assets/icons8-linkedin.svg";
 import gmail from "../assets/icons8-gmail.svg";
 import medium from "../assets/icons8-medium.svg";
 import lock from "../assets/icons8-lock-64.png";
+import { BsArrowUpRight } from "react-icons/bs";
 
 const Contacts = () => {
   // send mail function
@@ -14,8 +15,8 @@ const Contacts = () => {
 
     emailjs
       .sendForm(
-        "service_q2zcfdms",
-        "template_109uui5",
+        "service_q2zcfdm",
+        "template_zfotyiy",
         form.current,
         "4e9mL2GiajNKys8R5"
       )
@@ -31,43 +32,46 @@ const Contacts = () => {
 
   return (
     <div id="contact" className="my-36 pt-10">
-      {/* social links */}
-      <div className="my-60 py-20"></div>
-      {/* mail contact form */}
-      <div className="mt-14">
-        <div>
-          <h1 className="text-4xl font-bold text-slate-800 my-10">
-            Love to hear from you <br></br> Get in touch 👋
-          </h1>
-          {/* <p className="text-xs text-center">
-            Message's are encrypted, following<br></br> end to end encryption
-            mehtod
-          </p>
-          <img className="h-7 my-2" src={lock} alt="lock" /> */}
-        </div>
+      <div>
+        <h1 className="text-4xl font-bold text-slate-800 my-10">
+          Love to hear from you,<br></br> Get in touch 👋
+        </h1>
         <form className="flex flex-col" ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input
-            className="border border-slate-700 rounded p-1"
-            type="text"
-            name="user_name"
-          />
-          <label className="mt-3">Email</label>
-          <input
-            className="border border-slate-700 rounded p-1"
-            type="email"
-            name="user_email"
-          />
-          <label className="mt-3">Message</label>
+          <div className="flex items-center gap-4">
+            <div className="flex flex-col w-full">
+              <label className="mt-3 tracking-wide font-bold text-slate-800">
+                Your Name
+              </label>
+              <input
+                className="border border-slate-200sss bg-white rounded p-2 w-full"
+                type="text"
+                name="user_name"
+              />
+            </div>
+            <div className="flex flex-col w-full">
+              <label className="mt-3 tracking-wide font-bold text-slate-800">
+                Your Email
+              </label>
+              <input
+                className="border border-slate-200 bg-white p-2"
+                type="email"
+                name="user_email"
+              />
+            </div>
+          </div>
+          <label className="mt-3 tracking-wide font-bold text-slate-800">
+            Message
+          </label>
           <textarea
-            className="border border-slate-50 bg-slate-100 rounded p-2"
+            className="border border-slate-200 bg-white p-2"
             name="message"
           />
-          <input
-            className="mt-5 border border-slate-50 w-fit rounded-md pt-2 pb-1 px-5 bg-slate-900 text-white"
+          <button
+            className="mt-5 border border-slate-50 w-2/4 rounded pt-2 pb-1 px-5 bg-slate-800 text-white"
             type="submit"
-            value="Send"
-          />
+          >
+            Just Send <BsArrowUpRight className="inline-block"></BsArrowUpRight>
+          </button>
         </form>
       </div>
     </div>
--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -16,7 +16,7 @@ const Work = () => {
   };
 
   return (
-    <div className="my-20 pt-10" id="work">
+    <div className="my-36 pt-10" id="work">
       <h1 className="text-3xl font-bold">
         <span className="text-red-400">#</span>Some Things I've Built
       </h1>
