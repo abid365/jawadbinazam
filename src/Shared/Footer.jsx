43fa2faa42c853631c004becb4bf888f8012d430
jawad Bin Azam <abidibnazam@gmail.com>
2023-07-03 23:38:01 +0600

    added whatsapp link

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BsLinkedin, BsMedium, BsTwitter } from "react-icons/bs";
+import { BsLinkedin, BsMedium, BsTwitter, BsWhatsapp } from "react-icons/bs";
 import { FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
@@ -19,10 +19,14 @@ const Footer = () => {
         <a href="https://twitter.com/foxbate71">
           <BsTwitter className="inline-block text-white"></BsTwitter>
         </a>
+        <a href="https://wa.me/+8801828909335">
+          <BsWhatsapp className="inline-block text-white"></BsWhatsapp>
+        </a>
       </div>
       {/* location */}
       <div>
         <h1 className="text-white text-xs mt-2">Chattogram, Bangladesh</h1>
+        <h1 className="text-white text-xs mt-2">Phone: +8801828909335</h1>
       </div>
       {/* copyright */}
       <div>
