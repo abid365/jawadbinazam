411098e0cfab3d271cf4c73b6f86e543b616be1b
abid365 <abidibnazam@gmail.com>
2026-02-03 08:21:56 +0600

    add:reka ui

---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+<template>
+  <div>
+    <h1 class="text-sm">Hello world</h1>
+    <p class="text-sm">how to train your dragong</p>
+    <p>how are you mate?</p>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["reka-ui/nuxt"],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
